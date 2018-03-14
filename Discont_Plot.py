b52bfc66badf85c064ee02c8bdbe1524da7b3816
Marcus Hock <marcushock@localhost.localdomain>
2018-03-14 10:27:04 -0600

    Changes for multiple structures

--- Discont_Plot.py
+++ Discont_Plot.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+#Note: To specify a temperature use the -T flag followed by the tempearture to plot at
+#Note: To plot a second coordinate file, use the -S and specify the path to the new coordinate folder.
+
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -13,6 +16,8 @@ import ThermodynamicProperties as Pr
 
 parser = ArgumentParser()
 parser.add_argument('-T', dest = 'temp')
+parser.add_argument('-S', dest = 'second')
+parser.add_argument('-Th', dest = 'third')
 args = parser.parse_args()
 temp = args.temp
 if temp!= None:
@@ -21,7 +26,16 @@ else:
 	plot_temp = float(250)
 
 
-print 'Plotting at',plot_temp*1,' K.'
+
+second = args.second
+if second!= None:
+	second_dir = second;
+third = args.third
+if third !=None:
+	third_dir = third;
+
+
+print('Plotting at',plot_temp*1,' K.')
 
 molecule = "molecule.xyz"
 struct_vol = Pr.Volume(Program='Tinker', Coordinate_file=molecule)
@@ -37,22 +51,40 @@ my_Gs = raw[:, int(plot_temp)/10,2]
 
 os.chdir("Cords/")
 files = np.sort([i for i in os.listdir('.') if os.path.isfile(os.path.join('.',i)) and 'out' in i])
-#print files
 
 coordinates = np.zeros((len(files),n_atm, 3))
 volume = np.zeros(len(files))
 velocity = np.zeros((len(files)-1, n_atm))
 
+
 for i in range(len(files)):
-   coordinates[i] = Ex.Return_Tinker_Coordinates(files[i]) 
+   coordinates[i] = Ex.Return_Tinker_Coordinates(files[i])
    volume[i] = Pr.Volume(Program='Tinker', Coordinate_file=files[i])
    if i > 0:
        for j in range(n_atm):
            velocity[i - 1,j] = np.linalg.norm(coordinates[i,j] - coordinates[i-1,j])
+os.chdir("../")
 
- #float(str.split(molData[1])[0])*float(str.split(molData[1])[1])*float(str.split(molData[1])[2])
-
-#print(struct_vol)
+if second != None:
+	os.chdir(second_dir)
+	with open(molecule) as m:
+		molData2 = m.readlines()
+	raw2 = np.load('out_raw.npy')
+	my_volumes2 = raw2[:,int(plot_temp/10),6]
+	my_Gs2 = raw2[:, int(plot_temp)/10,2]
+	files2 = np.sort([i for i in os.listdir('.') if os.path.isfile(os.path.join('.',i)) and 'out' in i])
+	coordinates2 = np.zeros((len(files2),n_atm, 3))
+	volume2 = np.zeros(len(files2))
+if third != None:
+        os.chdir(third_dir)
+        with open(molecule) as m:
+                molData3 = m.readlines()
+        raw3 = np.load('out_raw.npy')
+        my_volumes3 = raw3[:,int(plot_temp/10),6]
+        my_Gs3 = raw3[:, int(plot_temp)/10,2]
+        files3 = np.sort([i for i in os.listdir('.') if os.path.isfile(os.path.join('.',i)) and 'out' in i])
+        coordinates3 = np.zeros((len(files3),n_atm, 3))
+        volume3 = np.zeros(len(files3))
 
 plt.rc('text', usetex=True)
 plt.rc('font', size=16)
@@ -65,11 +97,17 @@ plt.ylabel(r'\Delta x [Ang.]', size=20)
 plt.show()
 
 #plt.figure(figsize=(8,3))
-plt.scatter(my_volumes/struct_vol,my_Gs)
+p1 = plt.scatter(my_volumes/struct_vol,my_Gs)
+if second != None:
+	p2 = plt.scatter(my_volumes2/struct_vol,my_Gs2,color='red')
+	plt.legend([p1, p2],["Structure 1", "Structure 2"])
+if third != None:
+        p3 = plt.scatter(my_volumes3/struct_vol,my_Gs3,color='green')
+        plt.legend([p1, p2,p3],["Structure 1", "Structure 2", "Structure 3"])
+
 plot_title = 'Gibbs Free Energy at '+str(int(plot_temp))+' K'
 plt.title(plot_title, size=24)
 plt.xlabel(r'\Delta V / V', size=20)
 plt.ylabel('G (kcal/mol)', size = 20)
 plt.show()
 #plt.figure(figsize=(3,8))
-
